738f9a3dd3b4f84785f4997b195f1aca584fffc7
Paulo Galdo <hades_54st@hotmail.com>
2016-06-03 01:03:22 -0300

    servicios agregados

---
+++ scripts/angular/model/TipoService.js
@@ -0,0 +1,102 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+miAppHome.service('_tipoService', function ($http, $q, $rootScope, $cookies) {
+
+    this.getListaTipo = function () {
+        var datosRecu = null;
+        var deferred = $q.defer();
+        var token = $cookies.getObject('token');
+        var uri = 'https://tierradecoloresapi.herokuapp.com/tipo/list';
+        $http({
+            url: uri,
+            method: 'get',
+            headers: {
+                'Authorization': 'Bearer ' + token.data.access_token,
+                'Content-type': 'application/json'
+            }
+        }).then(function successCallback(response) {
+            datosRecu = response;
+            deferred.resolve(datosRecu);
+        }, function errorCallback(response) {
+            datosRecu = response;
+            deferred.resolve(datosRecu);
+        });
+        return deferred.promise;
+    };
+
+    this.addTipo = function (tipo) {
+        var datosRecu = null;
+        var deferred = $q.defer();
+        var token = $cookies.getObject('token');
+        var uri = 'https://tierradecoloresapi.herokuapp.com/tipo/add';
+        $http({
+            url: uri,
+            method: 'post',
+            data: angular.toJson(tipo),
+            headers: {
+                'Authorization': 'Bearer ' + token.data.access_token,
+                'Content-type': 'application/json'
+            }
+        }).then(function successCallback(response) {
+            datosRecu = response;
+            deferred.resolve(datosRecu);
+        }, function errorCallback(response) {
+            datosRecu = response;
+            deferred.resolve(datosRecu);
+        });
+        return deferred.promise;
+    };
+
+    this.updateTipo = function (tipo) {
+        var datosRecu = null;
+        var deferred = $q.defer();
+        var token = $cookies.getObject('token');
+        var uri = 'https://tierradecoloresapi.herokuapp.com/tipo/update';
+        $http({
+            url: uri,
+            method: 'post',
+            data: angular.toJson(tipo),
+            headers: {
+                'Authorization': 'Bearer ' + token.data.access_token,
+                'Content-type': 'application/json'
+            }
+        }).then(function successCallback(response) {
+            datosRecu = response;
+            deferred.resolve(datosRecu);
+        }, function errorCallback(response) {
+            datosRecu = response;
+            deferred.resolve(datosRecu);
+        });
+        return deferred.promise;
+    };
+
+    this.deleteTipo = function (tipo) {
+        var datosRecu = null;
+        var deferred = $q.defer();
+        var token = $cookies.getObject('token');
+        var uri = 'https://tierradecoloresapi.herokuapp.com/tipo/delete';
+        $http({
+            url: uri,
+            method: 'post',
+            data: angular.toJson(tipo),
+            headers: {
+                'Authorization': 'Bearer ' + token.data.access_token,
+                'Content-type': 'application/json'
+            }
+        }).then(function successCallback(response) {
+            datosRecu = response;
+            deferred.resolve(datosRecu);
+        }, function errorCallback(response) {
+            datosRecu = response;
+            deferred.resolve(datosRecu);
+        });
+        return deferred.promise;
+    };
+
+});
+
+
+
