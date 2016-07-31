17d975bd06d9c74f3cd7f7c1f74a7c8dfa6f301d
PauloGaldo <PauloGaldo@192.168.1.53>
2016-07-31 13:48:51 -0300

    script para generar el instalador

---
+++ win.js
@@ -0,0 +1,15 @@
+var electronInstaller = require('electron-winstaller');
+var path = require('path');
+resultPromise = electronInstaller.createWindowsInstaller({
+    appDirectory: './TierraDesktop-win32-ia32',
+    outputDirectory: './Installer',
+    authors: 'Paulo Galdo - Cósmica Tecnología.',
+    owners: 'Paulo Galdo - Cósmica Tecnología.',
+    description: 'Sistema de gestion comercial.',
+    version: '2.0',
+    setupIcon: path.join(__dirname, '/app.ico'),
+    loadingGif: path.join(__dirname, '/balls.gif'),
+    exe: 'TierraDesktop.exe',
+    noMsi: true
+});
+resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
