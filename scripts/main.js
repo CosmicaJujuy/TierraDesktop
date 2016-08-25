8084daa9ffda19769154a1fce8797a22bf50351e
PauloGaldo <PauloGaldo@192.168.1.53>
2016-08-25 01:01:10 -0300

    metodo para navegar entre tabs agregado

--- scripts/main.js
+++ scripts/main.js
@@ -701,10 +701,54 @@ var miAppHome = angular.module('tierraDeColoresApp', [
                         }
                     });
         })
-        .run(function ($rootScope) {
+        .run(function ($rootScope, hotkeys, $state) {
+            $rootScope.tabs = 0;
+            hotkeys.add({
+                combo: 'ctrl+tab',
+                description: 'Navegar tabs',
+                callback: function (event, hotkey) {
+                    if ($state.current.name === 'login') {
+                        event.preventDefault();
+                    } else {
+                        $rootScope.tabs++;
+                        if ($state.current.name === 'distribucion' ||
+                                $state.current.name === 'distribuir_productos' ||
+                                $state.current.name === 'facturas'
+                                ) {
+                            if ($rootScope.tabs === 4) {
+                                $rootScope.tabs = 0;
+                            }
+                        }
+                        if ($state.current.name === 'modificar-perfil' ||
+                                $state.current.name === 'factura' ||
+                                $state.current.name === 'reserva') {
+                            if ($rootScope.tabs === 3) {
+                                $rootScope.tabs = 0;
+                            }
+                        }
+                        if ($state.current.name === 'bancos' ||
+                                $state.current.name === 'tarjetas' ||
+                                $state.current.name === 'pagos' ||
+                                $state.current.name === 'productos' ||
+                                $state.current.name === 'panel_factura_producto' ||
+                                $state.current.name === 'categorias' ||
+                                $state.current.name === 'marcas' ||
+                                $state.current.name === 'tipos' ||
+                                $state.current.name === 'proveedores' ||
+                                $state.current.name === 'nota_credito' ||
+                                $state.current.name === 'transferencias'
+                                ) {
+                            if ($rootScope.tabs === 2) {
+                                $rootScope.tabs = 0;
+                            }
+                        }
+                    }
+                }
+            });
             $rootScope.$on('$stateChangeStart',
                     function (event, toState, toParams, fromState, fromParams) {
                         /*CAMBIO DE ESTADO*/
+                        $rootScope.tabs = 0;
                     });
             var session = require('electron').remote.session;
             var ses = session.fromPartition('persist:name');
