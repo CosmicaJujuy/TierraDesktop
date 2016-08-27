5369c545ed04b08e1a42697550f0cb2d6583c297
PauloGaldo <PauloGaldo@192.168.1.53>
2016-08-27 11:32:57 -0300

    refactoring, modulo

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('tierraDeColoresApp', [
+            'ngAnimate',
+            'ui.router',
+            'anim-in-out',
+            'ngMessages',
+            'toaster',
+            'ngCookies',
+            'ngFileUpload',
+            'LocalStorageModule',
+            'ngMaterial',
+            'ui.bootstrap',
+            'ngTable',
+            'ngDialog',
+            'kendo.directives',
+            'ngSanitize',
+            'ui.mask',
+            'cfp.hotkeys',
+            'hm.readmore',
+            'angular-loading-bar'
+        ]);
+})();
