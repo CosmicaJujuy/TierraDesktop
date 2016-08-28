faf67a077734baf12012cf4cd3847dacb3258fdc
PauloGaldo <PauloGaldo@192.168.1.53>
2016-08-28 00:52:02 -0300

    refactoring, cambio de nombre del archivo module

---
+++ scripts/app.module.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular
+            .module('tierraDeColoresApp', [
+                /*Angular modules*/
+                'ngAnimate',
+                'ngMessages',
+                'ngSanitize',
+                /*3er-party modules*/
+                'ui.router',
+                'anim-in-out',
+                'toaster',
+                'ngFileUpload',
+                'LocalStorageModule',
+                'ngMaterial',
+                'ui.bootstrap',
+                'ngTable',
+                'ngDialog',
+                'kendo.directives',
+                'ui.mask',
+                'cfp.hotkeys',
+                'hm.readmore',
+                'angular-loading-bar'
+            ]);
+})();
