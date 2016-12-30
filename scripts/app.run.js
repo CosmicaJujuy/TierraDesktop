567e50954ae39b7863604b0a4e29fcca68ff851c
PauloGaldo <PauloGaldo@192.168.1.53>
2016-12-30 01:11:36 -0300

    navegacion de tabs removida

--- scripts/app.run.js
+++ scripts/app.run.js
@@ -7,71 +7,8 @@
     run.$inject = ['$rootScope', 'hotkeys', '$state'];
 
     function run($rootScope, hotkeys, $state) {
-
-        var sudo = require('sudo-prompt');
-        var options = {
-            name: 'Electron'
-        };
-        var command = "wspooler.exe -p3 -c '9∟Z'";
-        sudo.exec(command, options, function (error, stdout, stderr) {
-            console.log(error, command);
-            console.log(stdout);
-            console.log(stderr);
-        });
-
         var session = require('electron').remote.session;
-        var ses = session.fromPartition('persist:name');
-        $rootScope.tabs = 0;
-
-        hotkeys.add({
-            combo: 'ctrl+tab',
-            description: 'Navegar tabs',
-            callback: function (event, hotkey) {
-                if ($state.current.name === 'login') {
-                    event.preventDefault();
-                } else {
-                    $rootScope.tabs++;
-                    if ($state.current.name === 'distribucion' ||
-                            $state.current.name === 'distribuir_productos' ||
-                            $state.current.name === 'facturas'
-                            ) {
-                        if ($rootScope.tabs === 4) {
-                            $rootScope.tabs = 0;
-                        }
-                    }
-                    if ($state.current.name === 'modificar-perfil' ||
-                            $state.current.name === 'factura' ||
-                            $state.current.name === 'reserva') {
-                        if ($rootScope.tabs === 3) {
-                            $rootScope.tabs = 0;
-                        }
-                    }
-                    if ($state.current.name === 'bancos' ||
-                            $state.current.name === 'tarjetas' ||
-                            $state.current.name === 'pagos' ||
-                            $state.current.name === 'productos' ||
-                            $state.current.name === 'panel_factura_producto' ||
-                            $state.current.name === 'categorias' ||
-                            $state.current.name === 'marcas' ||
-                            $state.current.name === 'tipos' ||
-                            $state.current.name === 'proveedores' ||
-                            $state.current.name === 'nota_credito' ||
-                            $state.current.name === 'transferencias'
-                            ) {
-                        if ($rootScope.tabs === 2) {
-                            $rootScope.tabs = 0;
-                        }
-                    }
-                }
-            }
-        });
-
-        $rootScope.$on('$stateChangeStart',
-                function (event, toState, toParams, fromState, fromParams) {
-                    /*CAMBIO DE ESTADO*/
-                    $rootScope.tabs = 0;
-                });
-
+        var ses = session.fromPartition('persist:name'); 
         ses.clearCache(function (response) {
             /*console.log("Cache eliminado: ", response);*/
         });
