8e7d45a9a0d690cd0d24365a22d6a70e02f98643
Paulo <Paulo@Asus>
2017-07-20 02:38:39 -0300

    Tamaño de contenedor dinamico

--- scripts/app.run.js
+++ scripts/app.run.js
@@ -4,23 +4,26 @@
             .module('tierraDeColoresApp')
             .run(run);
 
-    run.$inject = ['$rootScope', 'hotkeys', '$state', '$window'];
+    run.$inject = ['$rootScope', 'hotkeys', '$state', '$window', '$location'];
 
-    function run($rootScope, hotkeys, $state, $window) {
+    function run($rootScope, hotkeys, $state, $window, $location) {
         var session = require('electron').remote.session;
         var ses = session.fromPartition('persist:name');
         ses.clearCache(function (response) {
             /*console.log("Cache eliminado: ", response);*/
         });
-        console.log();
-        if($window.innerHeight === 730){
-            $rootScope.height = "87.9%";
-        }else if($window.innerHeight === 1042){
-            $rootScope.height = "952px";
-        }
+        $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, options) {
+            if ($window.innerHeight === 730) {
+                $rootScope.height = "87.5%";
+            } else if ($window.innerHeight > 1000) {
+                $rootScope.height = "950px";
+            } else {
+                $rootScope.height = "87.5%";
+            }
+        });
         ses.cookies.get({name: 'token'}, function (error, cookies) {
-            /* console.log(cookies[0]);
-             console.log(cookies[0].value);*/
+            console.log(cookies[0]);
+            console.log(cookies[0].value);
         });
 
     }
