f6def194654e71a7b6c0f369fdba68bd8228dbe8
Paulo <Paulo@Asus>
2017-07-19 22:21:04 -0300

    Heigth dinamico

--- scripts/app.run.js
+++ scripts/app.run.js
@@ -4,15 +4,20 @@
             .module('tierraDeColoresApp')
             .run(run);
 
-    run.$inject = ['$rootScope', 'hotkeys', '$state'];
+    run.$inject = ['$rootScope', 'hotkeys', '$state', '$window'];
 
-    function run($rootScope, hotkeys, $state) {
+    function run($rootScope, hotkeys, $state, $window) {
         var session = require('electron').remote.session;
-        var ses = session.fromPartition('persist:name'); 
+        var ses = session.fromPartition('persist:name');
         ses.clearCache(function (response) {
             /*console.log("Cache eliminado: ", response);*/
         });
-
+        console.log();
+        if($window.innerHeight === 730){
+            $rootScope.height = "87.9%";
+        }else if($window.innerHeight === 1042){
+            $rootScope.height = "952px";
+        }
         ses.cookies.get({name: 'token'}, function (error, cookies) {
             /* console.log(cookies[0]);
              console.log(cookies[0].value);*/
