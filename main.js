9832c1446078178daed5e86cebf1ebc377964d88
Paulo <Paulo@Asus>
2017-12-02 22:34:31 -0300

    [FISCAL] Inicio y fin de proceso de servicio para impresion fiscal

--- main.js
+++ main.js
@@ -1,48 +1,34 @@
-/* global __dirname, process */
-
-const {
-    ipcMain,
-    Menu,
-    shell,
-    app,
-    BrowserWindow
-} = require('electron');
+/* global __dirname, printerProcess */
+const {ipcMain, Menu, shell, app, BrowserWindow} = require('electron');
 // this should be placed at top of main.js to handle setup events quickly
 if (handleSquirrelEvent()) {
     // squirrel event handled and app will exit in 1000ms, so don't do anything else
     return;
 }
-
 function handleSquirrelEvent() {
     if (process.argv.length === 1) {
         return false;
     }
-
     const ChildProcess = require('child_process');
     const path = require('path');
-
     const appFolder = path.resolve(process.execPath, '..');
     const rootAtomFolder = path.resolve(appFolder, '..');
     const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
     const exeName = path.basename(process.execPath);
-
     const spawn = function (command, args) {
         let spawnedProcess, error;
-
         try {
             spawnedProcess = ChildProcess.spawn(command, args, {
                 detached: true
             });
         } catch (error) {
+            console.log(error);
         }
-
         return spawnedProcess;
     };
-
     const spawnUpdate = function (args) {
         return spawn(updateDotExe, args);
     };
-
     const squirrelEvent = process.argv[1];
     switch (squirrelEvent) {
         case '--squirrel-install':
@@ -51,33 +37,25 @@ function handleSquirrelEvent() {
             // - Add your .exe to the PATH
             // - Write to the registry for things like file associations and
             //   explorer context menus
-
             // Install desktop and start menu shortcuts
             spawnUpdate(['--createShortcut', exeName]);
-
             setTimeout(app.quit, 1000);
             return true;
-
         case '--squirrel-uninstall':
             // Undo anything you did in the --squirrel-install and
             // --squirrel-updated handlers
-
             // Remove desktop and start menu shortcuts
             spawnUpdate(['--removeShortcut', exeName]);
-
             setTimeout(app.quit, 1000);
             return true;
-
         case '--squirrel-obsolete':
             // This is called on the outgoing version of your app before
             // we update to the new version - it's the opposite of
             // --squirrel-updated
-
             app.quit();
             return true;
     }
 }
-
 //Importing modules to print and manage the app
 
 var os = require("os");
@@ -101,12 +79,12 @@ function createWindow() {
         show: false,
         icon: __dirname + '/styles/images/app.png'
     });
-    //mainWindow.setIcon( __dirname + '/styles/images/appIcon.png');
 
     mainWindow.loadURL(`file://${__dirname}/index.html`);
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
     });
+
     mainWindow.on('closed', function () {
         var windows = BrowserWindow.getAllWindows();
         windows.forEach(function (value) {
@@ -126,13 +104,30 @@ function createWindow() {
     workerWindow.loadURL(`file://${__dirname}/worker.html`);
     workerWindow.hide();
 }
+var child_process = require('child_process');
+var printerProcess;
+app.on('ready', function () {
+    printerProcess = child_process.spawn('java', ['-jar', '.\\printer_service\\HasarPrinterAPI-0.5.jar']);
+    printerProcess.stdout.on('data', function (data) {
+        console.log("STDOUT: " + data);
+    });
+    printerProcess.stderr.on('data', function (data) {
+        console.log("STDERR: " + data);
+    });
+    printerProcess.on('close', function (data) {
+        console.log("CLOSE: " + data);
+    });
+});
+app.on('quit', function () {
+    printerProcess.kill('SIGINT');
+});
 app.on('ready', createWindow);
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
+    if (printerProcess.platform !== 'darwin') {
+        printerProcess.kill('SIGINT');
         app.quit();
     }
 });
-
 app.on('activate', function () {
     if (mainWindow === null) {
         createWindow();
